960106066e9c7231a58d737b553a724c895ac0e1
AmarMusic91 <music.amar@hotmail.com>
2021-03-12 22:17:13 +0100

    Mistar is the best!

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var someAdjective = "the best!";
+  var myString = "Mistarl is ";
+  myString += someAdjective;
+  return myString;
+}
+
+console.log(main());
+module.exports = main;
