1742ec7cb0d99d35a7484a1f6a93d5dcaea115d6
AmarMusic91 <music.amar@hotmail.com>
2021-03-12 23:21:14 +0100

    teslA

---
+++ ex12/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var lastName = "Tesla";
+  var thirdLetterOfLastName = lastName[lastName.length - 1];
+
+  return thirdLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
