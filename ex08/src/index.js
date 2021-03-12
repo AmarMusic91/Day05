76f0b2ca5b073569daecdddcd3ea4d1581369de8
AmarMusic91 <music.amar@hotmail.com>
2021-03-12 22:37:40 +0100

    .lenght

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    
+    var lastName = "Doe";
+    var lastNameLenght;
+
+  lastNameLenght = lastName.length;
+
+    return lastNameLenght;
+  }
+  
+  console.log(main());
+  module.exports = main;
